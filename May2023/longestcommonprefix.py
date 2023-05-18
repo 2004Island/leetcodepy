30d780e51d864c9e5c10731f4034d0fe906390c8
2004Island <varun@firecrest.org>
2023-05-18 01:18:08 -0700

    Second leetcode

---
+++ May2023/longestcommonprefix.py
@@ -0,0 +1,42 @@
+# Second LC problem May 18th
+# Apparently this answer is extremely inefficient so I may rework it in future
+
+
+class Solution:
+
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        
+        listofsplitwords = [[*x] for x in strs]
+        returnstr = ""
+        condition = True
+        testvar = True
+        
+        while condition:
+
+            for i in range(len(listofsplitwords[0])): 
+                for j in listofsplitwords: # iterates through each word in list of words
+                    if i >= len(j): # if iterator is greater than any other word len that means the current returnstr is the lcp 
+                        return returnstr
+                    else:
+                        if listofsplitwords[0][i] == j[i]: # if the nth letter of the first word is the same as the nth letter of the jth word that makes it common
+                            continue
+                        else:
+                            testvar = False
+                if testvar:
+                    returnstr += listofsplitwords[0][i]
+            
+            return returnstr
+
+        
+
+test = Solution()
+
+# Input: strs = ["flower","flow","flight"]
+strs = ["flower","flow","flight"]
+print(test.longestCommonPrefix(strs))
+# Returns "fl"
+
+# Input: strs = ["dog","racecar","car"]
+strs = ["dog","racecar","car"]
+print(test.longestCommonPrefix(strs))
+# Returns ""
--- May2023/romantoint.py
+++ May2023/romantoint.py
@@ -1,13 +1,14 @@
 # First LC problem May 18th
 
 
-class Solution(object):
+class Solution:
 
     def romanToInt(self, s: str) -> int:
 
         sum = 0
         values = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         splitlis = [*s]
+        
         for i in range(len(splitlis)):
 
             if i < len(splitlis) - 1:
